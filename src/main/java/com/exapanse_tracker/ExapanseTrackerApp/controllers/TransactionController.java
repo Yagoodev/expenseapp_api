83f74ff01269e838aa2c2f271237db0e61f1e4c9
Yagoodev <yago.yaguinho@live.com>
2025-01-22 22:49:38 -0300

    Feat: Retorno mapeado da listagem de transações

--- src/main/java/com/exapanse_tracker/ExapanseTrackerApp/controllers/TransactionController.java
+++ src/main/java/com/exapanse_tracker/ExapanseTrackerApp/controllers/TransactionController.java
@@ -2,6 +2,7 @@ package com.exapanse_tracker.ExapanseTrackerApp.controllers;
 
 import com.exapanse_tracker.ExapanseTrackerApp.domain.transaction.Transaction;
 import com.exapanse_tracker.ExapanseTrackerApp.dto.CreateTransactionRequestDTO;
+import com.exapanse_tracker.ExapanseTrackerApp.dto.ListTransactionsResponseDTO;
 import com.exapanse_tracker.ExapanseTrackerApp.services.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -10,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/transactions")
@@ -26,13 +28,32 @@ public class TransactionController {
     }
 
     @GetMapping("")
-    public ResponseEntity<List<Transaction>> list() {
+    public ResponseEntity<Map<String, List<ListTransactionsResponseDTO>>> list() {
 
         List<Transaction> transactionList = this.transactionService.listAllTransactions();
 
         HttpHeaders responseHeaders = new HttpHeaders();
         responseHeaders.set("Content-type", "application/json");
 
-        return ResponseEntity.status(HttpStatus.OK).headers(responseHeaders).body(transactionList);
+        List<ListTransactionsResponseDTO> transactionListParsed = transactionList.stream().map(transaction -> new ListTransactionsResponseDTO(
+                        transaction.getTransactionId(),
+                        transaction.getCreatedAt(),
+                        "",
+                        "",
+                        "",
+                        transaction.getAmount(),
+                        transaction.getType(),
+                        1,
+                        "",
+                        false,
+                        false,
+                        false,
+                        false
+                ))
+                .toList();
+
+        Map<String, List<ListTransactionsResponseDTO>> response = Map.of("result", transactionListParsed);
+
+        return ResponseEntity.status(HttpStatus.OK).headers(responseHeaders).body(response);
     }
 }
--- src/main/java/com/exapanse_tracker/ExapanseTrackerApp/dto/ListTransactionsResponseDTO.java
+++ src/main/java/com/exapanse_tracker/ExapanseTrackerApp/dto/ListTransactionsResponseDTO.java
@@ -1,4 +1,21 @@
 package com.exapanse_tracker.ExapanseTrackerApp.dto;
 
-public record ListTransactionsResponseDTO() {
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public record ListTransactionsResponseDTO(
+        Long id,
+        LocalDateTime date,
+        String institution,
+        String account,
+        String merchant,
+        BigDecimal amount,
+        String type,
+        int categoryId,
+        String category,
+        Boolean isPending,
+        Boolean isTransfer,
+        Boolean isExpense,
+        Boolean isEdited
+) {
 }
